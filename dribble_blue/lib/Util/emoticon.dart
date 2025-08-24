00c727a25a4575d897d18c622dba7c2f5c8b2359
TousifMitul <tousif5robbani@gmail.com>
2025-08-24 23:15:11 +0600

    new project

---
+++ dribble_blue/lib/Util/emoticon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Emoticon extends StatelessWidget {
+  const Emoticon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
