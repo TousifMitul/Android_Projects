b3d5d3dec623e1e009893c49ed51b8befe3b523e
TousifMitul <tousif5robbani@gmail.com>
2025-08-25 21:07:04 +0600

    new project

--- dribble_blue/lib/Util/emoticon.dart
+++ dribble_blue/lib/Util/emoticon.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 class Emoticon extends StatelessWidget {
-  const Emoticon({Key? key}) : super(key: key);
+  final String emoticon;
+  const Emoticon({
+    Key? key,
+ required this.emoticon
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blue[600],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: EdgeInsets.all(12),
+      child: Center(
+        child:
+      Text(
+        emoticon,
+          style: TextStyle(
+          fontSize:28,
+      ),
+      ),
+      ),
+    );
   }
 }
