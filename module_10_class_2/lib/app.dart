18fc179b52f42163adb2c194df2da4a0fba96697
TousifMitul <tousif5robbani@gmail.com>
2025-08-31 21:25:41 +0600

    class added

---
+++ module_10_class_2/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'Button_group.dart';
+import 'home.dart';
+import 'input_flutter.dart';
+
+class MyApp extends StatelessWidget{
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext contex){
+    return MaterialApp(
+      title: 'Batch-12',
+      home: FlutterInput(),
+    );
+  }
+}
